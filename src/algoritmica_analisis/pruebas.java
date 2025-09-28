f6f8c2ba1480357f7050acf43bba1e050ca75547
ivan <ivangd456@gmail.com>
2025-09-28 21:32:23 +0200

    añado fichero de prueba.

--- src/algoritmica_analisis/pruebas.java
+++ src/algoritmica_analisis/pruebas.java
@@ -4,6 +4,9 @@ public class pruebas {
 
     static void main() {
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+
     }
 
 }
